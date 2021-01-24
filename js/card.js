f6ce9bd4b820de79eaa7ee49a78ec87c63943b31
terstained <terstained@users.noreply.github.com>
2021-01-24 18:23:55 +0800

    Create card.js

---
+++ js/card.js
@@ -0,0 +1,7 @@
+/* card.js */
+
+// Runs every time card is clicked.
+// Visible effects only seen when card is clicked on non-hover devices. (mobile)
+function opencard(elt) {
+    elt.classList.toggle('open');
+}
